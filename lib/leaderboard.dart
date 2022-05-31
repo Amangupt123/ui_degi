2b7772142bad9ad671a376d65b504d83775a2bae
Anmolsri12 <anmolsrivastava60669@gmail.com>
2022-05-31 09:32:25 +0530

    aaaaaaajjjjjjj

--- lib/leaderboard.dart
+++ lib/leaderboard.dart
@@ -44,9 +44,10 @@ class _LeaderboardState extends State<Leaderboard> with SingleTickerProviderStat
         elevation: 0,
       ),
       body: Container(
-          height: screenSize.height,
-          width: screenSize.width,
-          child: Column(children: [
+        height: screenSize.height,
+        width: screenSize.width,
+        child: Column(
+          children: [
             Container(
               width: MediaQuery.of(context).size.width,
               height: 100,
@@ -85,6 +86,7 @@ class _LeaderboardState extends State<Leaderboard> with SingleTickerProviderStat
                           border: Border.all(width: 4.5, color: Colors.orange)),
                       labelColor: Colors.orange,
                       unselectedLabelColor: Colors.white,
+
                       tabs: const [
                         // first tab [you can add an icon using the icon property]
                         Tab(
@@ -101,62 +103,103 @@ class _LeaderboardState extends State<Leaderboard> with SingleTickerProviderStat
                   const SizedBox(
                     height: 35,
                   ),
-                  Row(
-                    children: [
-                      Column(
-                        children: [
-                          Container(
-                            height: 80,
-                            width: 80,
-                            decoration: const BoxDecoration(
-                                color: Colors.orange,
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                  image: AssetImage("assets/image/Ellipse 4@3x.png"),
-                                  fit: BoxFit.fitWidth,
-                                )),
-                          )
-                        ],
-                      ),
-                      
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 80,
-                            width: 80,
-                            decoration: const BoxDecoration(
-                                color: Colors.orange,
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                  image: AssetImage("assets/image/Ellipse 3@3x.png"),
-                                  fit: BoxFit.fitWidth,
-                                )),
-                          )
-                        ],
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 80,
-                            width: 80,
-                            decoration: const BoxDecoration(
-                                color: Colors.orange,
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                  image: AssetImage("assets/image/Ellipse@3x.png"),
-                                  fit: BoxFit.fitWidth,
-                                )),
-                          )
-                        ],
-                      )
-                    ],
+                  SizedBox(
+                    height: 174,
+                    width: MediaQuery.of(context).size.width * 335,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        SizedBox(
+                          height: 125,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Container(
+                                height: 80,
+                                width: 80,
+                                decoration: const BoxDecoration(
+                                    color: Colors.orange,
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                      image: AssetImage("assets/image/Ellipse 4@3x.png"),
+                                      fit: BoxFit.fitWidth,
+                                    )),
+                              ),
+                              Text(
+                                'Alexa key',
+                                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: Color(0xff000000)),
+                              ),
+                              Text('837 pts'),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 145,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 80,
+                                width: 80,
+                                decoration: const BoxDecoration(
+                                    color: Colors.orange,
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                      image: AssetImage("assets/image/Ellipse 3@3x.png"),
+                                      fit: BoxFit.fitWidth,
+                                    )),
+                              ),
+                              Text('Name'),
+                              Text('23 stf'),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 125,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Container(
+                                height: 80,
+                                width: 80,
+                                decoration: const BoxDecoration(
+                                  color: Colors.orange,
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    image: AssetImage("assets/image/Ellipse@3x.png"),
+                                    fit: BoxFit.fitWidth,
+                                  ),
+                                ),
+                              ),
+                              Text('Name'),
+                              Text('23 stf'),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
-          ])),
+            Expanded(
+                child: ListView.builder(
+              itemCount: 6,
+              itemBuilder: (context, i) {
+                return ListTile(
+                  title: Text("numberTruthList"),
+                );
+                Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.red,
+                );
+              },
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
